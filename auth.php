039bbeee6d605b4ba926767ce98cec6a12d1f6e3
Кирилл Малышев <63231735+MalyshevKirill@users.noreply.github.com>
2022-03-25 02:22:39 +0300

    Add cookies and captcha

--- auth.php
+++ auth.php
@@ -1,20 +1,30 @@
 <?php
-    if(isset($_POST["login"])) 
+    function auth($login, $password) 
     {
         $isSuccess = 0;
         $connect = new PDO("mysql:host=127.0.0.1:3306, dbname=test", "root", "");
-        $login = $_POST['login'];
         $result = $connect->query("SELECT * FROM test.users WHERE login='$login'")->fetch();
         if (!empty($result)) {
             $hashedPassword = $result["password"];
-            if (password_verify($_POST["password"], $hashedPassword)) {
+            if (password_verify($password, $hashedPassword)) {
                 $isSuccess = 1;
             }
         }
         if ($isSuccess == 0) {
             echo "Error";
         } else {
+            setcookie('auth', json_encode(array('login' => $login, 'password' => $password)));
             echo "Success";
+        }
+    }
+
+    if(isset($_POST["login"])) 
+    {
+        auth($_POST['login'], $_POST['password']);
+    } else {
+        if(isset($_COOKIE['auth'])) {
+            $data = json_decode($_COOKIE['auth']);
+            auth($data->login, $data->password);
         }
     }
 ?>
--- register.php
+++ register.php
@@ -1,22 +1,33 @@
 <?php
-    if(isset($_POST["login"])) 
-    {
-        $connect = new PDO("mysql:host=127.0.0.1:3306, dbname=test", "root", "");
-        $data = array(
-            ':name' => $_POST["name"],
-            ':surname' => $_POST["surname"],
-            ':email' => $_POST["email"],
-            ':login' => $_POST["login"],
-            ':password' => password_hash($_POST["password"], PASSWORD_DEFAULT),
-            ':gender' => $_POST["gender"],
-            ':over18yo' => $_POST["over18yo"]
-        );
-        $query = "INSERT INTO test.users
-        (name, surname, email, login, password, gender, over18yo) 
-        VALUES (:name, :surname, :email, :login, :password, :gender, :over18yo)
-        ";
-        $statement = $connect->prepare($query);
-        $statement->execute($data);
-        echo "Data saved";
+    $secret = '6LftzwofAAAAAE5uuiBWAu5g8U4UTciu6_uwQeTf';
+    require_once (dirname(__FILE__).'/recaptcha/src/autoload.php');
+    if (isset($_POST['g-recaptcha-response'])) {
+        $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+        $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+        if ($resp->isSuccess()) {
+            $connect = new PDO("mysql:host=127.0.0.1:3306, dbname=test", "root", "");
+            $data = array(
+                ':name' => $_POST["name"],
+                ':surname' => $_POST["surname"],
+                ':email' => $_POST["email"],
+                ':login' => $_POST["login"],
+                ':password' => password_hash($_POST["password"], PASSWORD_DEFAULT),
+                ':gender' => $_POST["gender"],
+                ':over18yo' => $_POST["over18yo"]
+            );
+            $query = "INSERT INTO test.users
+            (name, surname, email, login, password, gender, over18yo) 
+            VALUES (:name, :surname, :email, :login, :password, :gender, :over18yo)
+            ";
+            $statement = $connect->prepare($query);
+            $status = $statement->execute($data);
+            if($status == 1) {
+                echo "Success";
+            } else {
+                echo "Error";
+            }
+        } else {
+            echo "Captcha error";
+        }
     }
 ?>
